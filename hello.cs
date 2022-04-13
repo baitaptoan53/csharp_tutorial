f4ebca22592fe86ac71fbac11e96ab4f640ec42b
baitaptoan53 <baitaptoan53@gmail.com>
2022-04-13 17:49:11 +0700

    a

---
+++ hello.cs
@@ -0,0 +1,62 @@
+//object oriented programming
+//class
+//object
+//method
+//constructor
+//property
+//field
+ //create a class
+                   //create a constructor
+                   //create a method
+                   //create a property
+                   //create a field
+                   //create an object
+                   //call the method
+                   //call the property
+//write a class
+//write a constructor
+//write a method
+using System;
+//write a class name
+public class Hello
+{
+    //write a constructor name
+    public Hello()
+    {
+        //write a method name
+
+                   }
+                   public void SayHello()
+                   {
+                       //write a property name
+                       //write a field name
+                       Console.WriteLine("Hello World");
+                   }
+                   public void SayHello(string name)
+                   {
+                       //write a property name
+                       //write a field name
+                       Console.WriteLine("Hello " + name);
+                   }
+                   public void SayHello(string name, int age)
+                   {
+                       //write a property name
+                       //write a field name
+                       Console.WriteLine("Hello " + name + " you are " + age);
+                   }
+
+                   public void SayHello(string name, int age, string job)
+                   {
+                       //write a property name
+                       //write a field name
+                       Console.WriteLine("Hello " + name + " you are " + age + " and you are a " + job);
+                   }
+                   
+        //write a property name
+        //write a field name
+        //write an object name
+        //call the method
+        //call the property
+        //call the field
+    }
+}
